e555e37fdfee28747504006662df7933b5222180
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-10-22 11:26:51 -0500

    fix: installing some dependencies.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,33 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import routerMain from './routes';
+import routerEvents from './context/events/routes';
+import cors from 'cors';
 
 import { errorHandler } from './shared/middleware';
 
 const app = express();
+
+const whiteList = ['http://localhost:5173', 'https://example.com'];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || whiteList.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(routerMain);
+app.use(routerEvents);
 app.use(errorHandler);
 export default app;
