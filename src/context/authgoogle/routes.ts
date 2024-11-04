1c0926d5496d0d5e89236c8c266e3a9fe40d6687
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-11-04 17:34:02 -0500

    fix: I commented the authGoogle folder because we are not using that yet.

--- src/context/authgoogle/routes.ts
+++ src/context/authgoogle/routes.ts
@@ -1,16 +1,16 @@
-import Router from 'express';
-import {
-  generateNewRefreshToken,
-  signInWithGoogle,
-  signInWithGoogleCallback,
-  verifyTokenFromGoogle,
-} from './controller';
+// import Router from 'express';
+// import {
+//   generateNewRefreshToken,
+//   signInWithGoogle,
+//   signInWithGoogleCallback,
+//   verifyTokenFromGoogle,
+// } from './controller';
 
-const router = Router();
+// const router = Router();
 
-router.get('/loginwithgoogle', signInWithGoogle);
-router.get('/v1/callback', signInWithGoogleCallback);
-router.get('/verify/:token', verifyTokenFromGoogle);
-router.get('/generate-refresh-token/:refresh_token', generateNewRefreshToken);
+// router.get('/loginwithgoogle', signInWithGoogle);
+// router.get('/v1/callback', signInWithGoogleCallback);
+// router.get('/verify/:token', verifyTokenFromGoogle);
+// router.get('/generate-refresh-token/:refresh_token', generateNewRefreshToken);
 
-export default router;
+// export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,13 @@ import AuthenticationRouter from '../context/auth/routes';
 import EventsRouter from '../context/events/routes';
 import UserRouter from '../context/users/routes';
 import PaymentsRouter from '../context/payments/routes';
-import LoginWithGoogleRouter from '../context/authgoogle/routes';
+// import LoginWithGoogleRouter from '../context/authgoogle/routes';
 import UploadImageRouter from '../uploadImages/routes';
 
 const router = Router();
 
 router.use('/auth', AuthenticationRouter);
-router.use('/auth', LoginWithGoogleRouter);
+// router.use('/auth', LoginWithGoogleRouter);
 router.use(EventsRouter);
 router.use(UserRouter);
 router.use('/payments', PaymentsRouter);
